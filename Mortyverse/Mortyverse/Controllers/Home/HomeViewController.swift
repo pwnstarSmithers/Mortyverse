8a71e6826a5d67288960e378c6864813f16581c1
Smith Yusuf Mugalu <yusuf.smith@equitybank.co.ke>
2024-09-07 12:28:20 +0300

    setup collectionviews

---
+++ Mortyverse/Mortyverse/Controllers/Home/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  Mortyverse
+//
+//  Created by Mugalu on 07/09/2024.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+    
+}
---
+++ Mortyverse/Mortyverse/View/cells/CharacterCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterCollectionViewCell.swift
+//  Mortyverse
+//
+//  Created by Mugalu on 07/09/2024.
+//
+
+import UIKit
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+    
+}
